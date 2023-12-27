677c598583bb8d8878a33bb36507cb4707767411
Hassaan <98661403+hazzySoomro@users.noreply.github.com>
2023-12-27 20:53:13 +0500

    a

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,8 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
-import Header from '@/components/header'
+import Header from '@/components/header1'
 import Footer from '@/components/footer'
+import ActiveSectionContextProvider from '../context/active-section-context';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,15 +15,14 @@ export default function RootLayout({ children }) {
   return (
     
     <html lang="en">
-      <body  className="bg-black text-gray-400 ">
+      <body  className="bg-black text-gray-200 ">
+        <ActiveSectionContextProvider>
         <div className='flex items-center justify-center bg-slate-200'>
-
         <Header />
         </div>
         {children}
         <Footer/>
-       
-       
+        </ActiveSectionContextProvider>
         </body>
     </html>
   )
--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
-
 import Image from 'next/image'
+import Intro from '@/components/intro'
 
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <h2>hello</h2>
-    {/* <Header/> */}
+    <main className="flex min-h-screen flex-col items-center justify-between ">
+    <div className='bg-gray-600 rounded-sm pb'>
+    <Intro/>
+    </div>
+    
     </main>
   )
 }
