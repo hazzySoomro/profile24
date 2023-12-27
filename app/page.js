2483aa7746daefccd050cf298e42ae96eb7ff30b
Hassaan <98661403+hazzySoomro@users.noreply.github.com>
2023-12-27 21:27:26 +0500

    a

--- app/page.js
+++ app/page.js
@@ -1,6 +1,9 @@
+"use client"
+
 import Image from 'next/image'
 import Intro from '@/components/intro'
 import SectionDivider from '@/components/section-divider'
+import { motion } from "framer-motion";
 
 
 export default function Home() {
@@ -8,8 +11,13 @@ export default function Home() {
     <main className="flex flex-col items-center px-4">
     <div className='bg-gray-600 rounded-md'>
     <Intro/>
-    <SectionDivider className="bg-white"/>
-    <h2>hell</h2>
+    <motion.div
+      className="bg-gray-300 my-24 h-16 w-1 rounded-full flex ml-[400px] items-center"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.125 }}
+    ></motion.div>
+
     </div>
     
     </main>
