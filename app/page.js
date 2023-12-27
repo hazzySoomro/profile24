c24b6e6c9893855955f53912cd57bacda509e241
Hassaan <98661403+hazzySoomro@users.noreply.github.com>
2023-12-27 21:10:12 +0500

    a

--- app/page.js
+++ app/page.js
@@ -1,12 +1,15 @@
 import Image from 'next/image'
 import Intro from '@/components/intro'
+import SectionDivider from '@/components/section-divider'
 
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between ">
-    <div className='bg-gray-600 rounded-sm pb'>
+    <main className="flex flex-col items-center px-4">
+    <div className='bg-gray-600 rounded-md'>
     <Intro/>
+    <SectionDivider className="bg-white"/>
+    <h2>hell</h2>
     </div>
     
     </main>
