ff29f75103a64f9554683acd1dd12dab51517ac8
Hassaan <98661403+hazzySoomro@users.noreply.github.com>
2023-12-27 20:13:26 +0500

    exzt

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     
     <html lang="en">
-      <body className={inter.className}>
+      <body className={inter.className} className="bg-black text-gray-400">
         <Header/>
         {children}
         <Footer/>
